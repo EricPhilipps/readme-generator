f62ecda52227fa252ead5c06373bbefccfec9b7c
Eric <ericphilipps123@gmail.com>
2022-04-13 18:20:46 -0500

    Added Questions Array

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,18 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    'What would you like the title of your README to be?',
+    'What is the description of your program?',
+    'What installation instructions would you like to include?',
+    'What usage information would you like to include?',
+    'What contribution guidelines would you like to include?',
+    'What test instructions would youb like to include?',
+    'What license badge would you like to include?',
+    'What is your GitHub username?',
+    'What is your email address?'
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
